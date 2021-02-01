7399ac1e29fe98fb3bb07a785e8029b88b58ce67
신범식 <@DESKTOP-96LDN4O>
2021-02-01 10:21:50 +0900

    MISO revised

--- cdd.m
+++ cdd.m
@@ -12,7 +12,7 @@ cl=3;                                 % constraint length
 code_rate=1/2;                        % code rate
                              
 SNR=0:3:30;
-Iteration=1000;
+Iteration=5000;
 
 %% CDD
 for SNR_index=1:length(SNR)
@@ -20,24 +20,29 @@ for SNR_index=1:length(SNR)
         %% MISO-OFDM (CDD와 성능 비교대상)
         Data = randi([0 1],[N Data_Size]);     
         mod_data=base_mod(Data,Modulation_Order);                           % 변조 방식에 따른 데이터 변조
-        IFFT_data=ifft(mod_data)*sqrt(FFT_Size);                            % 변조된 데이터 IFFT연산(각 반송파에서 보내는 신호의 power를 1로 하기위해 sqrt(반송파 개수)를 곱해줌
+        IFFT_data=ifft(mod_data,FFT_Size,2)*sqrt(FFT_Size);                 % 변조된 데이터 IFFT연산(각 반송파에서 보내는 신호의 power를 1로 하기위해 sqrt(반송파 개수)를 곱해줌
         Add_CP_data=[IFFT_data(:,FFT_Size-GI_Size+1:end), IFFT_data];       % IFFT연산된 데이터에 CP삽입
-
-        for Num_channel=1:N                                                 % 송신 안테나 갯수만큼 채널 생성
-            
-            h(Num_channel,:)=rayleigh_channel(Multi_path);          % time domain channel
-            H(Num_channel,:)=fft(h(Num_channel,:),FFT_Size);        % frequency domain channel
-            
+ 
+        h=rayleigh_channel([Multi_path N]);                                 % time domain channel
+        H=fft(h,FFT_Size,2);                                                % frequency domain channel
+        
+        for Num_channel=1:N
             hx(Num_channel,:)=conv(Add_CP_data(Num_channel,:),h(Num_channel,:));  % 전송된 데이터가 다중경로 채널 통과(h*x)
-            y(Num_channel,:)=awgn_noise(hx(Num_channel,:),SNR(SNR_index)); % 채널 통과된 데이터에 awgn추가 (y=h*x+n)
-            
-            y_remove_CP(Num_channel,:)=y(Num_channel,GI_Size+1:GI_Size+FFT_Size);      %remove CP
-            Y(Num_channel,:)=fft(y_remove_CP(Num_channel,:),FFT_Size)/sqrt(FFT_Size);  % CP제거된 데이터 FFT연산(신호의 power를 1로 하기 위해 sqrt(반송파 개수)로 나눠줌)
-            Y_equalize(Num_channel,:)=Y(Num_channel,:)./H(Num_channel,:); 
-            Y_demod(Num_channel,:)=base_demod(Y_equalize(Num_channel,:),Modulation_Order);                    % equalize된 데이터를 복조 
+        end
+        
+        y=awgn_noise(hx,SNR(SNR_index));                                    % 채널 통과된 데이터에 awgn추가 (y=h*x+n)
+        y_remove_CP=y(:,GI_Size+1:GI_Size+FFT_Size);                        %remove CP
+        Y=fft(y_remove_CP,FFT_Size,2)/sqrt(FFT_Size);                       % CP제거된 데이터 FFT연산(신호의 power를 1로 하기 위해 sqrt(반송파 개수)로 나눠줌)
+        Y_equalize=Y./H; 
+        Y_demod=base_demod(Y_equalize,Modulation_Order);                 % equalize된 데이터를 복조 
             
 
-        end
-            num_error(Iter,SNR_index)=biterr(Data,Y_demod);                     % 각 SNR당 비트오류갯수를 Iteration마다 배열에 저장
+
+        num_error(Iter,SNR_index)=biterr(Data,Y_demod);                     % 각 SNR당 비트오류갯수를 Iteration마다 배열에 저장
     end
 end
+error_rate=(sum(num_error,1)/(Data_Size*N))/Iteration;
+
+semilogy(SNR,error_rate,'-o')
+title('BER Performance'), xlabel('SNR(dB)'),ylabel('BER')
+grid on
