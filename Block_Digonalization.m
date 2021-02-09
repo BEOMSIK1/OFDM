79526876547f5b5df643b42adeb70250dc607c9d
신범식 <@DESKTOP-96LDN4O>
2021-02-09 20:20:08 +0900

    revised

---
+++ Block_Digonalization.m
@@ -0,0 +1,37 @@
+clc, clear, close all;
+%% Parameters
+Nt = 4;                                    % 송신안테나 개수
+Nr = 2;                                    % 수신안테나 개수 / 유저
+K = Nt/Nr;                                 % 유저의 수
+modulation_order = 4;                      % 1:BPSK  2:QPSK  4: 16QAM  6: 64QAM  8: 256QAM
+symbol_size = 128;
+data_size = symbol_size*modulation_order;
+noise_power = 1;
+P=0:3:30;                                  % transmit power
+SNR=0;
+iteration=1000;
+%% Block Digonalization
+for power_index=1:length(P)
+    for iter=1:iteration
+        %% Channel genrate
+        H=(randn(Nr*K,Nt)+j*randn(Nr*K,Nt))/sqrt(2);
+        %% Data
+        X=randi([0 1], [Nt, data_size]);                                    % N_t개의 서로 다른 데이터 (송신측)
+        X_mod = base_mod(X, modulation_order);                              % modulation
+        %% BD
+        [Y_demod,S]=MU_BD(P,power_index,X_mod,H,SNR,modulation_order,Nt,Nr);
+        num_error(iter,power_index)=biterr(X,Y_demod);                    % x,y의 총 error 발생 횟수
+        %% Sum rate
+        SR(iter,power_index)=S;    
+    end
+end
+error_rate=(sum(num_error,1)/(data_size*Nt))/iteration;
+sum_rate=(sum(abs(SR),1))/iteration;
+%% graph
+semilogy(P,error_rate,'-o')
+title('BER Performance'), xlabel('Transmit Power(dB)'),ylabel('BER'),legend('BD')
+grid on
+figure
+plot(P,sum_rate,'-o')
+title('Sum Rate'), xlabel('Transmit Power(dB)'),ylabel('Sum Rate(bps/Hz'),legend('BD')
+grid on
---
+++ Channel_Inversion.m
@@ -0,0 +1,36 @@
+clc, clear, close all;
+%% Parameters
+Nt = 4;                                     % 송신안테나 개수
+Nr = 1;                                     % 수신안테나 개수 / 유저
+K = Nt/Nr;                                  % 유저의 수
+modulation_order = 4;                       % 1:BPSK  2:QPSK  4: 16QAM  6: 64QAM  8: 256QAM
+symbol_size = 128;
+data_size = symbol_size*modulation_order;
+P=0:3:30;                                   % power
+SNR=0;
+iteration=10000;
+%% Channel inversion
+for power_index=1:length(P)
+    for iter=1:iteration
+        %% Channel genrate
+        H=(randn(Nr*K,Nt)+j*randn(Nr*K,Nt))/sqrt(2);
+        %% Data
+        X=randi([0 1], [Nt, data_size]);                                    % N_t개의 서로 다른 데이터 (송신측)
+        X_mod = base_mod(X, modulation_order);                              % modulation
+        %% ZF
+        Y_demod_ZF=MU_ZF(P,power_index,X_mod,H,SNR,modulation_order);
+        %% MMSE
+        Y_demod_MMSE=MU_MMSE(P,power_index,X_mod,H,SNR,modulation_order,K,Nt);
+        %% Error
+        num_error_ZF(iter,power_index)=biterr(X,Y_demod_ZF);                      % x,y의 총 error 발생 횟수
+        num_error_MMSE(iter,power_index)=biterr(X,Y_demod_MMSE); 
+    end
+end
+error_rate_ZF=(sum(num_error_ZF,1)/(data_size*Nt))/iteration;
+error_rate_MMSE=(sum(num_error_MMSE,1)/(data_size*Nt))/iteration;
+%% graph
+semilogy(P,error_rate_ZF,'-o')
+hold on
+semilogy(P,error_rate_MMSE,'-*')
+title('BER Performance'), xlabel('Transmit Power(dB)'),ylabel('BER'),legend('ZF','MMSE')
+grid on
