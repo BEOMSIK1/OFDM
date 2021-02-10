5dd4cf52063dcf30d0bd1de098cfd218f03ad7ae
신범식 <@DESKTOP-96LDN4O>
2021-02-10 16:09:19 +0900

    revised

--- MU_BD.m
+++ MU_BD.m
@@ -6,7 +6,7 @@ for i=1:K
     H_tilde=H;
     H_tilde(idx-(Nr-1):idx,:) = [];                               % i 번째 채널을 제외한 H_i 생성
     [~,~,V] = svd(H_tilde);                                       % singular value decomposition
-    F(:,idx-(Nr-1):idx) = V(Nt-Nr+1:Nt,:)';                       % precoding matrix (F_j)
+    F(:,idx-(Nr-1):idx) = V(:,Nt-Nr+1:Nt);                       % precoding matrix (F_j)
 end
 %% Tx-Rx
 X_power=1*(10^(P(power_index)/10));                               % rho 값 
